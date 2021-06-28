8929ac49d2e46e63891a89f1edbca1db718702f5
Tanishq-Jainop <78198324+Tanishq-Jainop@users.noreply.github.com>
2021-06-28 20:32:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function adduser()
+{
+player1=document.getElementById("player1").value;
+player2=document.getElementById("player2").value;
+localStorage.setItem("player1",player1);
+localStorage.setItem("player2",player2);
+window.location.replace("game_page.html")
+}
